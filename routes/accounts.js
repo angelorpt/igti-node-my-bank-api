76e4851ff47809ae4de63fdb74f4e32f7a4165a2
Angelo Rafael <angelorpt@gmail.com>
2021-08-17 20:25:40 -0400

    add | Método GET by ID

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,9 +3,34 @@ import config from "config";
 import { promises as fs } from "fs";
 
 const { readFile, writeFile } = fs;
-
 const router = express.Router();
 
+// GET
+router.get("/", async (req, res) => {
+  try {
+    const dbFile = JSON.parse(await readFile(config.get("accounts.file")));
+    res.send(dbFile.accounts);
+  } catch (error) {
+    res.send({ error }, 400);
+  }
+});
+
+// GET ID
+router.get("/:id", async (req, res) => {
+  try {
+    const ID = req.params.id;
+    const dbFile = JSON.parse(await readFile(config.get("accounts.file")));
+    const account = dbFile.accounts.find((account) => account.id == ID);
+    if (!account) {
+      res.send({ message: "Não encontrado" }, 404);
+    }
+    res.send(account);
+  } catch (error) {
+    res.send({ error }, 400);
+  }
+});
+
+// POST
 router.post("/", async (req, res) => {
   try {
     let account = req.body;
@@ -21,15 +46,6 @@ router.post("/", async (req, res) => {
     res.send(account, 201);
   } catch (error) {
     res.status(400).send({ error });
-  }
-});
-
-router.get("/", async (req, res) => {
-  try {
-    const dbFile = JSON.parse(await readFile(config.get("accounts.file")));
-    res.send(dbFile.accounts);
-  } catch (error) {
-    res.send({ error }, 400);
   }
 });
 
